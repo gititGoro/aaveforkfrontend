e6701dd4ef56d6c3b744165fe05a06cb4511f05c
Justin Goro <justingoro@protonmail.com>
2020-07-19 18:41:28 +0200

    toggle breakpoints in leftpanel

--- client/src/components/Layout/LeftPanel/index.tsx
+++ client/src/components/Layout/LeftPanel/index.tsx
@@ -1,5 +1,98 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Drawer, Grid, Button } from '@material-ui/core'
+import { Theme, makeStyles, createStyles } from '@material-ui/core/styles';
+import withWidth from '@material-ui/core/withWidth';
+import { Breakpoint } from '@material-ui/core/styles/createBreakpoints';
 
-export default function LeftPanel(){
-    return <div>Left panel</div>
+let bigWidth = 180
+let smallWidth = 50
+
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        actionDrawerShrunk: {
+            width: smallWidth,
+            flexShrink: 0,
+        },
+        actionDrawerBig: {
+            width: bigWidth
+        },
+        actionDrawerPaperShrunk: {
+            width: smallWidth,
+        },
+        actionDrawerPaperBig: {
+            width: bigWidth
+        },
+        contentRoot: {
+        },
+        footer: {
+            position: "absolute",
+            bottom: 0,
+            right: 0,
+        }
+    }),
+);
+
+interface leftPanelProps {
+    width: Breakpoint
 }
+
+function LeftPanel(props: leftPanelProps) {
+    const classes = useStyles()
+    const [expanded, setExpanded] = useState<boolean>(true)
+    const [clickExpand, setClickExpand] = useState<boolean>(false)
+    const [clickShrink, setClickShrink] = useState<boolean>(false)
+
+    const wide = widthBig(props.width)
+
+    useEffect(() => {
+        if (clickExpand) {
+            setExpanded(true)
+            setClickShrink(false)
+        }
+        else if (clickShrink) {
+            setExpanded(false)
+            setClickExpand(false)
+        }
+
+        if (!wide && expanded && !clickExpand) {
+            setExpanded(false)
+        } else if (wide && !expanded && !clickShrink) {
+            setExpanded(true)
+        }else if(wide && expanded){
+            setClickExpand(false)
+        }
+    });
+
+    let drawerClass = expanded ? classes.actionDrawerBig : classes.actionDrawerShrunk
+    let drawerPaperClasses = expanded ? classes.actionDrawerPaperBig : classes.actionDrawerPaperShrunk
+
+    return <Drawer variant="persistent"
+        open={true}
+        className={drawerClass}
+        classes={{ paper: drawerPaperClasses }}
+    >
+        <div className={classes.contentRoot}>
+            <Grid
+                container
+                direction="column"
+                justify="flex-end"
+                alignItems="center"
+                className={classes.footer}
+            >
+                <Grid item>
+                    {expanded
+                        ?
+                        <Button onClick={() => setClickShrink(true)}>{"<<"}</Button>
+                        :
+                        <Button onClick={() => setClickExpand(true)}>{">>"} </Button>}
+                </Grid>
+            </Grid>
+        </div>
+    </Drawer>
+}
+
+function widthBig(width: Breakpoint): boolean {
+    return width === 'lg' || width === 'xl'
+}
+
+export default withWidth()(LeftPanel)
--- client/src/components/Layout/index.tsx
+++ client/src/components/Layout/index.tsx
@@ -13,7 +13,7 @@ export default function Layout(props: props) {
     // const ethereumContextProps = useContext(EthereumContext)
 
 
-    return <div>
+    return <div>   
         <LeftPanel />
         <BottomPanel />
         <PageContent />
